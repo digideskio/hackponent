f1909a3041180aa92f63d05aec923dfad338a9e8
Giuseppe Gurgone <ggurgone@yelp.com>
2014-11-06 20:28:58 +0100

    Styleguide initial commit

---
+++ styleguide/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require("gulp");
+var util = require("gulp-util");
+var plumber = require("gulp-plumber");
+var myth = require("gulp-myth");
+var watch = false; // to enable plumber just during watch
+
+gulp.task("myth-styles", function() {
+  // handle all CSS at the root of the folder as stylesheets
+  return gulp.src("./y/*.css")
+    .pipe(watch ? plumber() : util.noop())
+    .pipe(myth())
+    .pipe(gulp.dest("./y/dist/"));
+});
+
+gulp.task("myth", ["styles"], function() {
+  watch = true;
+  // watch all CSS to reload the task
+  gulp.watch("./y/*", ["myth-styles"]);
+});
